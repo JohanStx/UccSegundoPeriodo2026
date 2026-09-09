8a6411d6eaeacc0f5694578c6dae60186cf8bd69
JohanP <johan.zuluaga@campusucc.edu.co>
2026-09-08 20:19:14 -0500

    agrege semana 2

--- POO/semana1/Ejercicio1Semana1.java
+++ POO/semana1/Ejercicio1Semana1.java
@@ -1,6 +1,6 @@
-public class Ejercicio1 {
+public class Ejercicio1Semana1 {
     public static void main(String[] args) {
-        
+
         System.out.println("Programacion orientada a objetos");
         
     } 
