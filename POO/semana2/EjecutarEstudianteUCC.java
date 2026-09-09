b21e4829f711c3908ad97c214d56cb9ad48a563c
JohanP <johan.zuluaga@campusucc.edu.co>
2026-09-08 21:31:56 -0500

    Creacion de la clase EstudianteUCC

--- POO/semana2/EjecutarEstudianteUCC.java
+++ POO/semana2/EjecutarEstudianteUCC.java
@@ -4,7 +4,7 @@ public class EjecutarEstudianteUCC {
         /*Creacion del objeto objEstudiante1 que se crea a partir
         de la clase EstudianteUCC */
 
-        EstudianteUCC objEstudiante1 = new EstudianteUCC(987161, 11170220, "Sofia", "Pichimata", 15, 1.60);
+        EstudianteUCC objEstudiante1 = new EstudianteUCC(987161, 11170220, "Sofia", "Pichimata", 15, 1.61);
         EstudianteUCC objEstudiante2 = new EstudianteUCC(123456, 10618964, "Aldair", "Peña", 80, 1.75);
 
 
